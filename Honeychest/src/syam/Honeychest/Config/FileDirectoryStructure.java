8b29d813e664b2d254d4109d038b827d3bbbe0bd
syamn <admin@sakura-server.net>
2012-06-12 00:15:42 +0900

    言語ファイル用のディレクトリを管理するクラスファイルの追加: Add FileDirectoryStructure.class for manage files and directories.

---
+++ Honeychest/src/syam/Honeychest/Config/FileDirectoryStructure.java
@@ -0,0 +1,58 @@
+package syam.Honeychest.Config;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import syam.Honeychest.Honeychest;
+
+public class FileDirectoryStructure {
+	public final static Logger log = Honeychest.log;
+	private static final String logPrefix = Honeychest.logPrefix;
+	private static final String msgPrefix = Honeychest.msgPerfix;
+
+	private static File pluginDir = new File("plugins", "Honeychest");
+	private static File languageDir;
+
+	private static final String languageDirName = "lang";
+
+	/**
+	 * セットアップを行う
+	 */
+	public static void setup(){
+		// プラグインディレクトリ取得
+		pluginDir = Honeychest.getInstance().getDataFolder();
+		// 言語ディレクトリ取得
+		languageDir = new File(pluginDir, languageDirName);
+
+		// plugins/Honeychest
+		createDir(pluginDir);
+		// plugins/Honeychest/lang
+		createDir(languageDir);
+	}
+
+	/**
+	 * 存在しないディレクトリを作成する
+	 * @param dir File 作成するディレクトリ
+	 */
+	private static void createDir(File dir){
+		// 既に存在すれば作らない
+		if (dir.isDirectory()){
+			return;
+		}
+		if (!dir.mkdir()){
+			log.warning(logPrefix+ "Can't create directory: " + dir.getName());
+		}
+	}
+
+	/* 以下 getter */
+
+	public static File getJarFile(){
+		return new File("plugins","HoneyChest.jar");
+	}
+	public static File getPluginDirectory(){
+		return pluginDir;
+	}
+	public static File getLanguageDirectory(){
+		return languageDir;
+	}
+}
